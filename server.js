5900e7519ed6b499c83dd4f5a769c05bc4f4baed
prashantdawar <prashantdawar.2008@gmail.com>
2019-01-04 22:33:49 +0530

    service worker added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 
 app.use('/bower_components', express.static(`${__dirname}/bower_components/`));
 app.use('/js', express.static(`${__dirname}/js/`));
+app.use('/sw.js', express.static(`${__dirname}/sw.js`));
 app.use((req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(port, () => {
---
+++ sw.js
@@ -0,0 +1,55 @@
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+           .register('/sw.js')
+           .then(function() { console.log("Service Worker Registered"); })
+           .catch(err => console.log(err));
+}
+
+
+
+let cacheName = 'ltm-workspace_cache';
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(
+                [
+                "/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "/bower_components/select2/dist/css/select2.min.css",
+                "/bower_components/select2-bootstrap-theme/dist/select2-bootstrap.min.css",
+                "/bower_components/jquery/dist/jquery.slim.min.js",
+                "/bower_components/popper.js/dist/umd/popper.min.js",
+                "/bower_components/bootstrap/dist/js/bootstrap.min.js" ,
+                "/bower_components/select2/dist/js/select2.min.js" ,
+                "/bower_components/handlebars/handlebars.min.js" ,
+                "/bower_components/vanilla-router/dist/vanilla-router.min.js" ,
+                "/bower_components/pouchdb/dist/pouchdb.min.js" ,
+                "/bower_components/pouchdb/dist/pouchdb.find.min.js" ,
+                "/",
+                "/sw.js",
+                "/js/config.js" ,
+                "/js/views.js" ,
+                "/js/app.js" ,
+                "/js/modals.js" ,
+                "/js/db.js" ,
+                ]
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    self.skipWaiting();
+// console.log(event.request);
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            // switch(event.request.url){
+            //     case 'http://localhost:5984/uuid': return;
+            // }
+        return caches.match(event.request)
+            // .then(function(response) {
+            //     return response || fetch(event.request);
+            // });
+        })
+    );
+});
